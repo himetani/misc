b301dbec032fb80e86d6fa727971bf0cf960126c
himetani <takafumi_t1224@jcom.home.ne.jp>
2018-02-27 20:03:04 +0900

    default

---
+++ concurrent/default/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	done := make(chan interface{})
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		close(done)
+	}()
+
+	workCounter := 0
+loop:
+	for {
+		select {
+		case <-done:
+			break loop
+		default:
+		}
+
+		workCounter++
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	fmt.Printf("Achieved %v cycles of work before signalled to stop.\n", workCounter)
+}
