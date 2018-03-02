27497abbae25983dafdf236ef86b8f7752343cc2
himetani <takafumi_t1224@jcom.home.ne.jp>
2018-03-02 23:16:27 +0900

    pipelineBench

---
+++ concurrent/pipelineBench/pipeline.go
@@ -0,0 +1,49 @@
+package main
+
+func repeat(done <-chan interface{}, values ...interface{}) <-chan interface{} {
+	valueStream := make(chan interface{})
+	go func() {
+		defer close(valueStream)
+		for {
+			for _, v := range values {
+				select {
+				case <-done:
+					return
+				case valueStream <- v:
+				}
+			}
+		}
+	}()
+	return valueStream
+}
+
+func take(done <-chan interface{}, valueStream <-chan interface{}, num int) <-chan interface{} {
+	takeStream := make(chan interface{})
+	go func() {
+		defer close(takeStream)
+		for i := 0; i < num; i++ {
+			select {
+			case <-done:
+			case takeStream <- <-valueStream:
+			}
+		}
+	}()
+
+	return takeStream
+}
+
+func toString(done <-chan interface{}, valueStream <-chan interface{}) <-chan string {
+	stringStream := make(chan string)
+
+	go func() {
+		defer close(stringStream)
+		for v := range valueStream {
+			select {
+			case <-done:
+			case stringStream <- v.(string):
+			}
+		}
+	}()
+
+	return stringStream
+}
